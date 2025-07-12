1012ef597b6ae0c7ffc5ab612dd0e8c6d5547aea
AnushkaSingh <anushkasingh20156@gmail.com>
2025-07-12 13:17:10 +0530

    login and signup

---
+++ rewear/script.js
@@ -0,0 +1,39 @@
+// Handle Signup
+const signupForm = document.getElementById("signupForm");
+if (signupForm) {
+    signupForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    const user = { name, email, password };
+    localStorage.setItem("rewearUser", JSON.stringify(user));
+    alert("Signup successful! Please login.");
+    window.location.href = "login.html";
+    });
+}
+
+// Handle Login
+const loginForm = 
+document.getElementById("loginForm");
+if (loginForm) {
+    loginForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const email = document.getElementById("loginEmail").value;
+        
+    const password = document.getElementById("loginPassword").value;
+
+    const storedUser = JSON.parse(localStorage.getItem("rewearUser"));
+
+        if (storedUser && storedUser.email === email && storedUser.password === password) {
+            alert("Login successful!");
+            localStorage.setItem("loggedIn", "true");
+            window.location.href = "home.html";
+        } else {
+        alert("Invalid email or password.");
+        }
+    });
+} 
